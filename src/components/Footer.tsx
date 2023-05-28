f721445dd8654dcae5317bb91c97da363e389ced
Shoxrux Code <shoxruxpl04@gmail.com>
2023-05-28 16:34:42 +0500

    added homepage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface FooterProps {
 }
@@ -6,9 +7,58 @@ interface FooterProps {
 const Footer: React.FC<FooterProps> = () => {
 
    return (
-      <footer className="w-full mt-[100px] bg-[#BBBBBB]">
-         <div className="container mx-auto px-6 py-[70px]">
-            <h1>footer</h1>
+      <footer className="w-full mt-[100px] rounded-t-[16px] bg-[#BBBBBB]">
+         <div className="container mx-auto px-6 py-[70px] max-lg:py-[60px] flex max-md:flex-col items-start gap-20 max-lg:gap-14 max-md:gap-5 max-sm:gap-0">
+            <div className="w-1/2 h-full flex max-md:justify-start max-md:mb-5">
+               <Image className="" src={'/images/logo.png'} width={391} height={105} alt="logo" />
+            </div>
+            <div className="w-2/4 max-xl:w-3/5 max-lg:w-full">
+               <nav className="grid grid-cols-3 max-sm:grid-cols-2 gap-10 max-md:gap-20 max-sm:gap-5">
+                  <ul className="flex flex-col gap-3">
+                     <li className="text-2xl font-bold">Меню</li>
+                     <li className="hover:underline underline-offset-2">
+                        <Link href={'#'}>Каталог</Link>
+                     </li>
+                     <li className="hover:underline underline-offset-2">
+                        <Link href={'#'}>Водителям</Link>
+                     </li>
+                     <li className="hover:underline underline-offset-2">
+                        <Link href={'#'}>
+                           Частые вопросы
+                        </Link>
+                     </li>
+                     <li className="hover:underline underline-offset-2">
+                        <Link href={'#'}>
+                           О компании
+                        </Link>
+                     </li>
+                  </ul>
+                  <ul className="flex flex-col gap-3">
+                     <li className="text-2xl font-bold">Компания</li>
+                     <li className="hover:underline underline-offset-2">
+                        <Link href={'#'}>Правила пользователя</Link>
+                     </li>
+                     <li className="hover:underline underline-offset-2">
+                        <Link href={'#'}>Пользовательское соглашение</Link>
+                     </li>
+                  </ul>
+                  <ul className="flex flex-col gap-3">
+                     <li className="text-2xl font-bold">Соц сети</li>
+                     <li className="hover:underline underline-offset-2">
+                        <Link href={'#'}>Facebook</Link>
+                     </li>
+                     <li className="hover:underline underline-offset-2">
+                        <Link href={'#'}>Instagram</Link>
+                     </li>
+                     <li className="hover:underline underline-offset-2">
+                        <Link href={'#'}>Telegram</Link>
+                     </li>
+                     <li className="hover:underline underline-offset-2">
+                        <Link href={'#'}>Info@alidamdiy.uz</Link>
+                     </li>
+                  </ul>
+               </nav>
+            </div>
          </div>
       </footer>
    );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,52 @@
 import Image from "next/image";
+import Link from "next/link";
+import { BiSearch } from 'react-icons/bi'
 
 interface HeaderProps {
 }
-
 const Header: React.FC<HeaderProps> = ({ }) => {
 
    return (
       <header className="w-full bg-[#BBBBBB]">
-         <div className="container mx-auto px-6 py-7 bg-[#BBBBBB]">
-            <h1>header</h1>
+         <div className="container mx-auto px-6 py-7 flex items-center justify-between bg-[#BBBBBB]">
+            <div className="">
+               <Link href={'/'}>
+                  <Image src={'/images/logo.png'} width={170} height={45} alt="logo" />
+               </Link>
+            </div>
+            <div className="flex items-center">
+               <div className="relative">
+                  <BiSearch size={12} className="absolute top-2 left-1" />
+                  <input type="text" className="max-w-[180px] w-full px-6 border-b border-[#000] bg-transparent outline-transparent" />
+               </div>
+            </div>
+            <div className="">
+               <nav>
+                  <ul className="flex gap-8">
+                     <li className="font-medium text-[#474747]">
+                        <Link href={'#'}>Каталог</Link>
+                     </li>
+                     <li className="font-medium text-[#474747]">
+                        <Link href={'#'}>Водителям</Link>
+                     </li>
+                     <li className="font-medium text-[#474747]">
+                        <Link href={'#'}>О компании</Link>
+                     </li>
+                  </ul>
+               </nav>
+            </div>
+            <div className="">
+               <h1 className="font-bold text-[#E31E24]">+998 91 123 32 33</h1>
+            </div>
+            <div className="">
+               <select className="font-medium uppercase bg-transparent">
+                  <option>ru</option>
+                  <option>uz</option>
+               </select>
+            </div>
+            <div className="">
+               <button className="px-6 py-2 bg-[#E31E24] text-white">Сделать заказ</button>
+            </div>
          </div>
       </header>
    );
