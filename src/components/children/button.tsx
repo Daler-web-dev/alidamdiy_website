198826183bea466be8029b87f643c9d6919bac7b
jony <sabinalatipova20@gmail.com>
2023-05-29 21:24:19 +0500

    product cards were successfully added

---
+++ src/components/children/button.tsx
@@ -0,0 +1,13 @@
+interface ButtonProps {
+   children: string
+}
+
+const Button: React.FC<ButtonProps> = ({ children }) => {
+   return (
+      <>
+         <button className="font-medium leading-[150%] tracking-[-0.011em] px-6 py-2 rounded-[5px] ease-in duration-150 hover:shadow-[0_0_10px_#E31E24] bg-[#E31E24] text-white">{children}</button>
+      </>
+   );
+}
+
+export default Button;
