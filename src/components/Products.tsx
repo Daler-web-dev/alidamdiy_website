a267049f65bc2d8cc5b87cbfc32145194608fbed
jony <sabinalatipova20@gmail.com>
2023-05-29 19:36:17 +0500

    fixed

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -9,7 +9,7 @@ export interface IAppProps {
 
 const Products: React.FC<IAppProps> = ({ item }) => {
   console.log(item);
-  
+
   return (
     <div className="w-full h-[250px] bg-[#D9D9D9]">
       <img src={item?.img} alt="" />
