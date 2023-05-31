d3a2b131c2a38190ec35327c0893430974aac3c4
jony <sabinalatipova20@gmail.com>
2023-05-31 19:44:50 +0500

    header mb version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,65 +1,90 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { BiSearch } from "react-icons/bi";
+import { GiHamburgerMenu } from "react-icons/gi";
 import Button from "./children/Button";
 
-import { BiSearch } from "react-icons/bi";
+interface HeaderProps { }
 
-interface HeaderProps {}
+const Header: React.FC<HeaderProps> = ({ }) => {
 
-const Header: React.FC<HeaderProps> = ({}) => {
-  return (
-    <header className="w-full bg-[#BBBBBB]">
-      <div className="container mx-auto px-[100px] py-7 flex items-center justify-between bg-[#BBBBBB]">
-        <div className="">
-          <Link href={"/"}>
-            <Image
-              src={"/images/logo.png"}
-              width={170}
-              height={45}
-              alt="logo"
-            />
-          </Link>
-        </div>
-        <div className="flex items-center">
-          <div className="relative">
-            <BiSearch size={15} className="absolute top-1 left-1" />
-            <input
-              type="text"
-              className="max-w-[180px] w-full px-6 outline-none bg-transparent outline-transparent border-b  border-[#000]"
-            />
-          </div>
-        </div>
-        <div className="">
-          <nav>
-            <ul className="flex gap-8">
-              <li className="font-medium text-[#474747]">
-                <Link href={"/catalog"}>Каталог</Link>
-              </li>
-              <li className="font-medium text-[#474747]">
-                <Link href={"#"}>Водителям</Link>
-              </li>
-              <li className="font-medium text-[#474747]">
-                <Link href={"#"}>О компании</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div className="">
-          <h1 className="font-bold text-[#E31E24]">+998 91 123 32 33</h1>
-        </div>
-        <div className="">
-          <select className="font-medium uppercase bg-transparent">
-            <option>ru</option>
-            <option>uz</option>
-          </select>
-        </div>
-        <div className="">
-          <Button>Сделать заказ</Button>
-        </div>
-      </div>
-    </header>
-  );
+	return (
+		<header className="w-full bg-[#BBBBBB]">
+			<div className="container mx-auto px-24 max-xl:px-14 max-lg:px-6 py-7 flex items-center justify-between bg-[#BBBBBB]">
+				<div className="max-xl:hidden block">
+					<Link href={"/"}>
+						<Image
+							src={"/images/logo.png"}
+							width={170}
+							height={45}
+							alt="logo"
+						/>
+					</Link>
+				</div>
+				<div className="flex gap-5 max-sm:gap-2">
+					<button className="p-3 max-sm:p-2 rounded-md bg-[#E31E24]">
+						<BiSearch color="white" size={15} />
+					</button>
+					<button className="max-md:block hidden p-3 max-sm:p-2 rounded-md bg-[#E31E24]">
+						<GiHamburgerMenu color="white" size={15} />
+					</button>
+				</div>
+				<div className="flex items-center max-md:absolute left-0 -top-full">
+					<div className="max-xl:block hidden mr-10 max-lg:mr-5">
+						<h1 className="font-bold max-lg:text-sm text-[#E31E24]">+998 91 123 32 33</h1>
+					</div>
+					<div className="flex flex-col items-center gap-3">
+						<div className="max-xl:block hidden">
+							<Link href={"/"}>
+								<Image
+									src={"/images/logo.png"}
+									width={170}
+									height={45}
+									alt="logo"
+								/>
+							</Link>
+						</div>
+						<nav>
+							<ul className="flex gap-8 max-lg:gap-3">
+								<li className="font-medium max-lg:text-sm text-[#474747]">
+									<Link href={"/catalog"}>Каталог</Link>
+								</li>
+								<li className="font-medium max-lg:text-sm text-[#474747]">
+									<Link href={"#"}>Водителям</Link>
+								</li>
+								<li className="font-medium max-lg:text-sm text-[#474747]">
+									<Link href={"#"}>О компании</Link>
+								</li>
+							</ul>
+						</nav>
+					</div>
+					<div className="max-xl:hidden block ml-10">
+						<h1 className="font-bold text-[#E31E24]">+998 91 123 32 33</h1>
+					</div>
+				</div>
+				<div className="max-md:block hidden ">
+					<Link href={"/"}>
+						<Image
+							src={"/images/logo.png"}
+							width={132}
+							height={35}
+							alt="logo"
+						/>
+					</Link>
+				</div>
+				<div className="">
+					<select className="font-medium uppercase bg-transparent">
+						<option>ru</option>
+						<option>uz</option>
+					</select>
+				</div>
+				<div className="max-md:hidden">
+					<Button>Сделать заказ</Button>
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
