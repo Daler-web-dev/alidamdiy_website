afee6c4680b4d3476150aba2e08b02f659bd3450
Shoxrux Code <shoxruxpl04@gmail.com>
2023-06-07 21:37:31 +0500

    local path was removed

--- src/pages/catalog.tsx
+++ src/pages/catalog.tsx
@@ -22,7 +22,7 @@ export interface ICarsProducts {
 
 
 export const getStaticProps = async () => { 
-  const res = await axios.get('http://localhost:3000/api/carsApi') 
+  const res = await axios.get('https://alidamdiyindustry.com/api/carsApi') 
   
   console.log(res);
   
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -435,7 +435,7 @@ const Home: React.FC<IMainProps> = ({ data }: any) => {
 export default Home;
 
 export const getStaticProps = async () => { 
-  const res = await axios.get('http://localhost:3000/api/carsApi') 
+  const res = await axios.get('https://alidamdiyindustry.com/api/carsApi') 
   
   console.log(res);
   
